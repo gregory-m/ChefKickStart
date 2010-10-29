acef58b03574fb6a569527d755e1a491907f2c27
Gregory Man <gregory@wix.com>
2010-10-30 01:24:51 +0200

    Added sudo cookbook and added sudo users to sample app role

--- roles/5_minutes_blog.rb
+++ roles/5_minutes_blog.rb
@@ -1,7 +1,7 @@
 name "5_minutes_blog"
 description "My blog in 5 minutes on Rails + ree + passanger + apache + mysql"
 
-run_list("recipe[users::sysadmins]", "recipe[simple_application::default]")
+run_list("recipe[users::sysadmins]","recipe[sudo::default]" ,"recipe[simple_application::default]")
 
 default_attributes("apps" => [
                              "id" => "5_minutes_blog",
@@ -14,8 +14,11 @@ default_attributes("apps" => [
                                   "reconnect" => true,
                                   "username" => "5_minutes"
                                   }}
-                              ]
-                    )
- 
-
+                              ],
+                    "authorization" => {
+                      "sudo" => {
+                        "groups" => ["wheel", "sysadmin"],
+                        "users" => ["gregory", "labria"]
+                      }
+                    })
                                                   # 
