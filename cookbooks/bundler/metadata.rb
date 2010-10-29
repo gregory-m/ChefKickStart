b06e7a375fa5b22ee0359c808058adabbac74745
Gregory Man <gregory@wix.com>
2010-10-29 23:54:47 +0200

    Implemented bundler cookbook

---
+++ cookbooks/bundler/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Wix.com"
+maintainer_email "gregory@wix.com"
+license          "Apache 2.0"
+description      "Installs/Configures bundler"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
