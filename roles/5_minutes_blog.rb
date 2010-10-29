0e550fa932d7859a4810b80405b43da43b71d04c
Gregory Man <gregory@wix.com>
2010-10-29 20:27:56 +0200

    One more role fix

--- roles/5_minutes_blog.rb
+++ roles/5_minutes_blog.rb
@@ -3,15 +3,16 @@ description "My blog in 5 minutes on Rails + ree + passanger + apache + mysq"
 run_list("recipe[users::sysadmins]", "recipe[ruby_enterprise::default]", "recipe[passenger_enterprise::apache2]",
           "recipe[rails_enterprise::default]", "recipe[database::simple]")
 
-default_attributes("apps" => ["databases" => {"test" => {"myt" => "111"}}])
+default_attributes("apps" => ["databases" => 
+                             {"production" => {
+                                "adapter" => "mysql",
+                                "database" => "5_minutes_blog_production",
+                                "encoding" => "utf8",
+                                "password" => "awesome_password",
+                                "reconnect" => true,
+                                "username" => "5_minutes"
+                                }
+                              }])
  
 
                                                   # 
-                                                  # "production" => {
-                                                  #   "adapter" => "mysql",
-                                                  #   "database" => "5_minutes_blog_production",
-                                                  #   "encoding" => "utf8",
-                                                  #   "password" => "awesome_password",
-                                                  #   "reconnect" => true,
-                                                  #   "username" => "5_minutes"
-                                                  #   }
